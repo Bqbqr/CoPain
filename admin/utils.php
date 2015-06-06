c603ec072e7b03b184cd249fb0cb2bbd51a191e6
Bqbqr <flo@tartampion.com>
2015-06-07 00:27:49 +0200

    Refactor

--- admin/utils.php
+++ admin/utils.php
@@ -42,5 +42,39 @@ function date_fr()
   return $Jour[date("w")]." ".date("d")." ".$Mois[date("n")-1];
   }
   
+function date_fr_arg($date){
+	$jour = date('l',strtotime($date));
+	$mois = date('F',strtotime($date));
+	switch($jour) {
+		case 'Monday': $jour = 'Lundi'; break;
+		case 'Tuesday': $jour = 'Mardi'; break;
+		case 'Wednesday': $jour = 'Mercredi'; break;
+		case 'Thursday': $jour = 'Jeudi'; break;
+		case 'Friday': $jour = 'Vendredi'; break;
+		case 'Saturday': $jour = 'Samedi'; break;
+		case 'Sunday': $jour = 'Dimanche'; break;
+		default: $jour =''; break;
+	}
+	switch($mois) {
+		case 'January': $mois = 'Janvier'; break;
+		case 'February': $mois = 'Février'; break;
+		case 'March': $mois = 'Mars'; break;
+		case 'April': $mois = 'Avril'; break;
+		case 'May': $mois = 'Mai'; break;
+		case 'June': $mois = 'Juin'; break;
+		case 'July': $mois = 'Juillet'; break;
+		case 'August': $mois = 'Août'; break;
+		case 'September': $mois = 'Septembre'; break;
+		case 'October': $mois = 'Octobre'; break;
+		case 'November': $mois = 'Novembre'; break;
+		case 'December': $mois = 'Decembre'; break;
+		default: $mois =''; break;
+	}
+	$jour_nb = date('d',strtotime($date));
+	return $jour." ".$jour_nb." ".$mois;
+}
+  
+
+
 ?>
 
--- index.php
+++ index.php
@@ -246,6 +246,7 @@
 
 // on se connecte à MySQL 
 include('secure/config.php');
+include('admin/utils.php');
 $db=mysql_connect($SQLhost, $SQLlogin, $SQLpass) or die(mysql_error());
 
 // on sélectionne la base 
@@ -279,36 +280,7 @@ mysql_select_db('pain',$db);
     <h2>Commande de pain - Bread Order</h2>
     <h3>
         <?php
-        $today=date("m/d/Y");
-        $jour = date('l',strtotime($today . "+1 days"));
-        $mois = date('F',strtotime($today . "+1 days"));
-          switch($jour) {
-              case 'Monday': $jour = 'Lundi'; break;
-              case 'Tuesday': $jour = 'Mardi'; break;
-              case 'Wednesday': $jour = 'Mercredi'; break;
-              case 'Thursday': $jour = 'Jeudi'; break;
-              case 'Friday': $jour = 'Vendredi'; break;
-              case 'Saturday': $jour = 'Samedi'; break;
-              case 'Sunday': $jour = 'Dimanche'; break;
-              default: $jour =''; break;
-            }
-          switch($mois) {
-              case 'January': $mois = 'Janvier'; break;
-              case 'February': $mois = 'Février'; break;
-              case 'March': $mois = 'Mars'; break;
-              case 'April': $mois = 'Avril'; break;
-              case 'May': $mois = 'Mai'; break;
-              case 'June': $mois = 'Juin'; break;
-              case 'July': $mois = 'Juillet'; break;
-              case 'August': $mois = 'Août'; break;
-              case 'September': $mois = 'Septembre'; break;
-              case 'October': $mois = 'Octobre'; break;
-              case 'November': $mois = 'Novembre'; break;
-              case 'December': $mois = 'Decembre'; break;
-              default: $mois =''; break;
-            }
-           $jour_nb = date('d')+1;
-        Print("Pour le $jour $jour_nb $mois");
+          echo "Pour le ".date_fr_arg(date('Y-m-d',strtotime(date("y-m-d")  . "+1 days")));
         ?>
     </h3>
     <p>
