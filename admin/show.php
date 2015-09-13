e503fe6fd4d87601d20092df79d73984f495a49b
f.Trois <flo@tartampion.com>
2015-09-13 15:53:24 +0200

    Fix bug when empty tab

--- admin/show.php
+++ admin/show.php
@@ -135,112 +135,116 @@ $objets=array();
               // On récupère tous les articles normaux. Sans les options
               $req = mysqli_query($bdd,'SELECT numorder,name,pitch,nom,taken,sum(quantity) as quantity FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 GROUP BY nom,numorder ORDER BY name;') or die('Erreur SQL !'.mysql_error()); 
 
-              // remplit notre tableau
-              while($data = mysqli_fetch_assoc($req)){
-                $tab[$data['numorder']][$data['nom']]=$data['quantity'];
-                $tab[$data['numorder']]['pitch']=$data['pitch'];
-                $tab[$data['numorder']]['name']=$data['name'];
-                $tab[$data['numorder']]['taken']=$data['taken'];
-                $tab[$data['numorder']]['order']=$data['numorder'];
-              }
+              // Si aucun article, on s'arrête là, aucune commande pour le jour même.
+              if(mysqli_num_rows($req)){  
 
-              //Et là on récupère nos options. on actualise le tableau ensuite.
-              $req = mysqli_query($bdd,'SELECT numorder,name,pitch, sum(quantity) as quantity, choice FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() GROUP BY name,numorder,choice;') or die('Erreur SQL !'.mysql_error()); 
-              while($data = mysqli_fetch_assoc($req)){
-                if(array_key_exists($data['choice'], $tab[$data['numorder']]))
-                  $tab[$data['numorder']][$data['choice']]+=$data['quantity'];
-                else
-                  $tab[$data['numorder']][$data['choice']]=$data['quantity'];
-              }
-              //On récup aussi le total des commandes:
-              $req = mysqli_query($bdd,'SELECT numorder,sum(quantity*prix) as total FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 GROUP BY numorder ORDER BY name;') or die('Erreur SQL !'.mysql_error()); 
-              while($data = mysqli_fetch_assoc($req)){
-                $tab[$data['numorder']]['total']=$data['total'];
-              }
-
-              //Taken. C'EST PAS PROPRE DU TOUT.
-              foreach ($tab as $name => $data) {
-                if ($name=="0" || $data["taken"]==1) {
-                  continue;
+                // remplit notre tableau
+                while($data = mysqli_fetch_assoc($req)){
+                  $tab[$data['numorder']][$data['nom']]=$data['quantity'];
+                  $tab[$data['numorder']]['pitch']=$data['pitch'];
+                  $tab[$data['numorder']]['name']=$data['name'];
+                  $tab[$data['numorder']]['taken']=$data['taken'];
+                  $tab[$data['numorder']]['order']=$data['numorder'];
                 }
-                echo '<tr>';
-                echo '<td><button value="'.$data['order'].'" type="button" class="btn btn-success recup" aria-label="Right Align">Vendue</button></td>';
-                echo '<td>'.$data['name'].'</td>';
-                echo '<td>'.$data['pitch'].'</td>';
-                foreach ($objet as $obj) {
-                  if(array_key_exists ($obj , $data))
-                    echo '<td>'.$data[$obj].'</td>';
+
+                //Et là on récupère nos options. on actualise le tableau ensuite.
+                $req = mysqli_query($bdd,'SELECT numorder,name,pitch, sum(quantity) as quantity, choice FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() GROUP BY name,numorder,choice;') or die('Erreur SQL !'.mysql_error()); 
+                while($data = mysqli_fetch_assoc($req)){
+                  if(array_key_exists($data['choice'], $tab[$data['numorder']]))
+                    $tab[$data['numorder']][$data['choice']]+=$data['quantity'];
                   else
-                    echo '<td>-</td>';
+                    $tab[$data['numorder']][$data['choice']]=$data['quantity'];
                 }
-                echo '<td>'.$data['total'].' €</td>';
-                echo '<td><button value="'.$data['order'].'" name='.$data['name'].' pitch='.$data['pitch'].' type="button" class="btn btn-info redo"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button></td>';
+                //On récup aussi le total des commandes:
+                $req = mysqli_query($bdd,'SELECT numorder,sum(quantity*prix) as total FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 GROUP BY numorder ORDER BY name;') or die('Erreur SQL !'.mysql_error()); 
+                while($data = mysqli_fetch_assoc($req)){
+                  $tab[$data['numorder']]['total']=$data['total'];
+                }
 
+                //Taken. C'EST PAS PROPRE DU TOUT.
+                foreach ($tab as $name => $data) {
+                  if ($name=="0" || $data["taken"]==1) {
+                    continue;
+                  }
+                  echo '<tr>';
+                  echo '<td><button value="'.$data['order'].'" type="button" class="btn btn-success recup" aria-label="Right Align">Vendue</button></td>';
+                  echo '<td>'.$data['name'].'</td>';
+                  echo '<td>'.$data['pitch'].'</td>';
+                  foreach ($objet as $obj) {
+                    if(array_key_exists ($obj , $data))
+                      echo '<td>'.$data[$obj].'</td>';
+                    else
+                      echo '<td>-</td>';
+                  }
+                  echo '<td>'.$data['total'].' €</td>';
+                  echo '<td><button value="'.$data['order'].'" name='.$data['name'].' pitch='.$data['pitch'].' type="button" class="btn btn-info redo"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button></td>';
+
+                  echo '</tr>';
+
+                }
+                //Not taken
+                foreach ($tab as $name => $data) {
+                  if ($name=="0"  || $data["taken"]==0) {
+                    continue;
+                  }
+                  echo '<tr>';
+                  echo '<td><button value="'.$data['order'].'" type="button" class="btn btn-warning unrecup" aria-label="Right Align">Annuler</button></td>';
+                  echo '<td>'.$data['name'].'</td>';
+                  echo '<td>'.$data['pitch'].'</td>';
+                  foreach ($objet as $obj) {
+                    if(array_key_exists ($obj , $data))
+                      echo '<td>'.$data[$obj].'</td>';
+                    else
+                      echo '<td>-</td>';
+                  }
+                  echo '<td>'.$data['total'].' €</td>';
+                  echo '<td><button value="'.$data['order'].'" name='.$data['name'].' pitch='.$data['pitch'].' type="button" class="btn btn-info redo"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button></td>';
+                  echo '</tr>';
+
+                }
                 echo '</tr>';
 
-              }
-              //Not taken
-              foreach ($tab as $name => $data) {
-                if ($name=="0"  || $data["taken"]==0) {
-                  continue;
+                //REQUETE DE LA MORT QUI TUE
+                $req = mysqli_query($bdd,'select sum(qty) as total, nom FROM (
+                                            SELECT sum(quantity) as qty, choice as nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() GROUP BY name,pitch,choice
+                                            UNION ALL
+                                            SELECT sum(quantity) as qty, nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 GROUP BY nom
+                                          ) s GROUP BY nom;') or die('Erreur SQL !'.mysql_error());  
+
+                while($data = mysqli_fetch_assoc($req)){
+                  $tab['total'][$data['nom']]=$data['total'];
                 }
-                echo '<tr>';
-                echo '<td><button value="'.$data['order'].'" type="button" class="btn btn-warning unrecup" aria-label="Right Align">Annuler</button></td>';
-                echo '<td>'.$data['name'].'</td>';
-                echo '<td>'.$data['pitch'].'</td>';
+                echo '<tr><td></td><td></td><td>Total:</td>';
                 foreach ($objet as $obj) {
-                  if(array_key_exists ($obj , $data))
-                    echo '<td>'.$data[$obj].'</td>';
+                  if(array_key_exists ($obj , $tab['total']))
+                    echo '<td>'.$tab['total'][$obj].'</td>';
                   else
-                    echo '<td>-</td>';
+                    echo '<td>0</td>';
                 }
-                echo '<td>'.$data['total'].' €</td>';
-                echo '<td><button value="'.$data['order'].'" name='.$data['name'].' pitch='.$data['pitch'].' type="button" class="btn btn-info redo"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button></td>';
-                echo '</tr>';
+                echo '<td></td></tr>';
 
+                //REQUETE DE LA MORT QUI TUE
+                $req = mysqli_query($bdd,'select sum(qty) as total, nom FROM (
+                                            SELECT sum(quantity) as qty, choice as nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() AND taken=0 AND deleted=0 GROUP BY name,pitch,choice
+                                            UNION ALL
+                                            SELECT sum(quantity) as qty, nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 AND taken=0 GROUP BY nom
+                                          ) s GROUP BY nom;') or die('Erreur SQL !'.mysql_error()); 
+
+                while($data = mysqli_fetch_assoc($req)){
+                  $tab['restant'][$data['nom']]=$data['total'];
+                }
+                echo '<tr><td></td><td></td><td>Restant:</td>';
+                foreach ($objet as $obj) {
+                  if(array_key_exists ($obj , $tab['restant']))
+                    echo '<td>'.$tab['restant'][$obj].'</td>';
+                  else
+                    echo '<td>0</td>';
+                }
+                echo '<td></td></tr>';
               }
-              echo '</tr>';
-
-              //REQUETE DE LA MORT QUI TUE
-              $req = mysqli_query($bdd,'select sum(qty) as total, nom FROM (
-                                          SELECT sum(quantity) as qty, choice as nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() GROUP BY name,pitch,choice
-                                          UNION ALL
-                                          SELECT sum(quantity) as qty, nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 GROUP BY nom
-                                        ) s GROUP BY nom;') or die('Erreur SQL !'.mysql_error());  
-
-              while($data = mysqli_fetch_assoc($req)){
-                $tab['total'][$data['nom']]=$data['total'];
-              }
-              echo '<tr><td></td><td></td><td>Total:</td>';
-              foreach ($objet as $obj) {
-                if(array_key_exists ($obj , $tab['total']))
-                  echo '<td>'.$tab['total'][$obj].'</td>';
-                else
-                  echo '<td>0</td>';
-              }
-              echo '<td></td></tr>';
-
-              //REQUETE DE LA MORT QUI TUE
-              $req = mysqli_query($bdd,'select sum(qty) as total, nom FROM (
-                                          SELECT sum(quantity) as qty, choice as nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN objet obj on obj.id=oc.choice WHERE date=CURDATE() AND taken=0 AND deleted=0 GROUP BY name,pitch,choice
-                                          UNION ALL
-                                          SELECT sum(quantity) as qty, nom FROM orders o INNER JOIN ordercontent oc on oc.numorder=o.id INNER JOIN article a on a.id=oc.article WHERE date=CURDATE() AND deleted=0 AND taken=0 GROUP BY nom
-                                        ) s GROUP BY nom;') or die('Erreur SQL !'.mysql_error()); 
-
-              while($data = mysqli_fetch_assoc($req)){
-                $tab['restant'][$data['nom']]=$data['total'];
-              }
-              echo '<tr><td></td><td></td><td>Restant:</td>';
-              foreach ($objet as $obj) {
-                if(array_key_exists ($obj , $tab['restant']))
-                  echo '<td>'.$tab['restant'][$obj].'</td>';
-                else
-                  echo '<td>0</td>';
-              }
-              echo '<td></td></tr>';
-
 
               mysqli_close($bdd); 
+
               ?>
               
             </tbody>
